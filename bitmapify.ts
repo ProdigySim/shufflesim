8029880c467f055c5edb4a293e25700c51ca39ba
ProdigySim <prodigysim@gmail.com>
2026-02-05 21:48:17 -0600

    Implement 4x integer scaling

--- bitmapify.ts
+++ bitmapify.ts
@@ -32,13 +32,23 @@ const normalized = aggregate.map(col => col.map(x => x/max));
 
 // console.log(normalized);
 
-const dst = new PNG({ width: DIMENSION, height: DIMENSION});
-
-for (let y=0; y < dst.height; y++) {
-  for (let x = 0; x < dst.width; x++) {
-    const idx = (dst.width * y + x) << 2;
-    // console.log(idx, normalized[y][x] * 255);
-    dst.data[idx+3] = normalized[y][x] * 255;
+function writePixel(dst: PNG, y: number, x: number, value: number) {
+  // Got this math from pngjs readme
+  const idx = (dst.width * y + x) << 2;
+  // Write only alpha channel--other channels are 0
+  dst.data[idx+3] = value;
+}
+// 4x integer scaling
+const dst = new PNG({ width: DIMENSION * 4, height: DIMENSION * 4});
+for (let y=0; y < DIMENSION; y++) {
+  for (let x = 0; x < DIMENSION; x++) {
+    const value = normalized[y][x] * 255;
+    // write a 4x4 square in the output
+    for(let yOffset = 0; yOffset < 4; yOffset++) {
+      for(let xOffset = 0; xOffset < 4; xOffset++) {
+        writePixel(dst, (4*y)+yOffset, (4*x)+xOffset, value);
+      }
+    }
   }
 }
 dst.pack().pipe(fs.createWriteStream(INFILE.replace(".json", ".png")));
