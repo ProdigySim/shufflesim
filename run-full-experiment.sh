2d3b57580e36869ac0a3cd6e021096287b2f450d
ProdigySim <prodigysim@gmail.com>
2026-02-05 22:02:53 -0600

    Move results to subdir

--- run-full-experiment.sh
+++ run-full-experiment.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
+mkdir -p results
+pushd results
 for i in {0,1,2,3,4,5,6,7,8,9,10}; do
   echo "Generating 10k_${i}shuffle.json"
-  deno run -A nriffles.ts $i > "10k_${i}shuffle.json";
+  deno run -A ../nriffles.ts $i > "10k_${i}shuffle.json";
 done
 
 echo "Generating 10k_trueshuffle.json"
-deno run -A trueshuffle.ts > 10k_trueshuffle.json
+deno run -A ../trueshuffle.ts > 10k_trueshuffle.json
 
 for i in {0,1,2,3,4,5,6,7,8,9,10}; do
   echo "Generating 10k_${i}shuffle.png"
-  deno run -A bitmapify.ts "10k_${i}shuffle.json";
+  deno run -A ../bitmapify.ts "10k_${i}shuffle.json";
 done
 
 echo "Generating 10k_trueshuffle.png"
-  deno run -A bitmapify.ts "10k_trueshuffle.json";
+  deno run -A ../bitmapify.ts "10k_trueshuffle.json";
+
+popd
