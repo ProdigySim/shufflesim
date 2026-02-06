0059e4e7b40c59c6502d603caaf25096c509e81e
ProdigySim <prodigysim@gmail.com>
2026-02-05 21:31:35 -0600

    Might as well do up to 10

--- run-full-experiment.sh
+++ run-full-experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for i in {0,1,2,3,4,5,6,7,8}; do
+for i in {0,1,2,3,4,5,6,7,8,9,10}; do
   echo "Generating 10k_${i}shuffle.json"
   deno run -A nriffles.ts $i > "10k_${i}shuffle.json";
 done
@@ -9,7 +9,7 @@ done
 echo "Generating 10k_trueshuffle.json"
 deno run -A trueshuffle.ts > 10k_trueshuffle.json
 
-for i in {0,1,2,3,4,5,6,7,8}; do
+for i in {0,1,2,3,4,5,6,7,8,9,10}; do
   echo "Generating 10k_${i}shuffle.png"
   deno run -A bitmapify.ts "10k_${i}shuffle.json";
 done
